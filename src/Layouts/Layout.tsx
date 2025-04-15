f6ee0e0794b88d257313935d6d1a6a5c022016a5
Lovely.Value <aid.l.v.esd@gmail.com>
2025-04-15 18:47:45 +0300

    переключение страниц с помощью кнопки

--- src/Layouts/Layout.tsx
+++ src/Layouts/Layout.tsx
@@ -1,6 +1,6 @@
 import cn from 'classnames';
 import { useEffect, useState } from 'react';
-import { NavLink, Outlet, useLocation } from 'react-router-dom';
+import { NavLink, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import styles from './Layout.module.css';
 
 const Layout = () => {
@@ -19,6 +19,19 @@ const Layout = () => {
 	useEffect(() => {
 		setButtonText('Start');
 	}, [location]);
+
+	//Переключение страниц с помощью кнопки
+	const navigate = useNavigate();
+	const links = ['/', '/short-break', 'long-break'];
+	const changeLink = (link: string[]) => {
+		if (link[0] === location.pathname) {
+			return link[1];
+		} else if (link[1] === location.pathname) {
+			return link[2];
+		} else if (link[2] === location.pathname) {
+			return link[0];
+		}
+	};
 
 	return (
 		<div className={styles['layout']}>
@@ -64,6 +77,7 @@ const Layout = () => {
 					{buttonText}
 				</button>
 				<button
+					onClick={() => navigate(changeLink(links), { replace: false })}
 					type='button'
 					className={cn(styles['hidden__button'], {
 						[styles['show_button']]: buttonText === 'Pause',
