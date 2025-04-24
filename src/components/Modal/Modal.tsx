5de14f44888438e41e06677029c2cfda29fa7cbc
Lovely.Value <aid.l.v.esd@gmail.com>
2025-04-24 18:39:58 +0300

    сделан пробный дизайн модального окна

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -8,7 +8,66 @@ const Modal: React.FC<ModalProps> = ({ open }) => {
 			className={cn(styles['hidden__div'], {
 				[styles['show__div']]: open,
 			})}
-		></div>
+		>
+			<button type='button' className={styles['modal__button-close']}>
+				<img src='public\close-button.svg' alt='close' />
+			</button>
+			<p className={styles['modal__title']}>Set Time</p>
+			<ul className={styles['modal__list']}>
+				<li>
+					<p className={styles['modal__paragraph']}>Pomodor</p>
+					<div className={styles['modal__input-inner']}>
+						<input
+							className={styles['modal__input']}
+							name='pomodor'
+							id='1'
+							type='text'
+						/>
+						<button className={styles['modal__list-button']}>
+							<img src='public\check-button.svg' alt='check' />
+						</button>
+						<button className={styles['modal__list-button']}>
+							<img src='public\reset-button.svg' alt='reset' />
+						</button>
+					</div>
+				</li>
+				<li>
+					<p className={styles['modal__paragraph']}>Short Brake</p>
+					<div className={styles['modal__input-inner']}>
+						<input
+							className={styles['modal__input']}
+							name='short-brake'
+							id='2'
+							type='text'
+						/>
+						<button className={styles['modal__list-button']}>
+							<img src='public\check-button.svg' alt='check' />
+						</button>
+						<button className={styles['modal__list-button']}>
+							<img src='public\reset-button.svg' alt='reset' />
+						</button>
+					</div>
+				</li>
+				<li>
+					<p className={styles['modal__paragraph']}>Long Break</p>
+					<div className={styles['modal__input-inner']}>
+						<input
+							className={styles['modal__input']}
+							name='long-brake'
+							id='3'
+							type='text'
+						/>
+						<button className={styles['modal__list-button']}>
+							<img src='public\check-button.svg' alt='check' />
+						</button>
+						<button className={styles['modal__list-button']}>
+							<img src='public\reset-button.svg' alt='reset' />
+						</button>
+					</div>
+				</li>
+			</ul>
+			<button className={styles['modal__button']}>Reset</button>
+		</div>
 	);
 };
 
